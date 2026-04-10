6f2ae9b8a29303cab0fd88d1539dffd630cf2dc2
wangzun <wangzun0009@gmail.com>
2026-04-10 21:09:48 +0800

    update: remove RayTracingKHR capability and its extension from integration test shader

--- tests/integration-test/build.rs
+++ tests/integration-test/build.rs
@@ -3,9 +3,7 @@ use rust_gpu_bindless_shader_builder::{ShaderSymbolsBuilder, spirv_builder::Capa
 fn main() -> anyhow::Result<()> {
 	ShaderSymbolsBuilder::new("integration-test-shader", "spirv-unknown-vulkan1.2")?
 		.capability(Capability::RayQueryKHR)
-		.capability(Capability::RayTracingKHR)
 		.extension("SPV_KHR_ray_query")
-		.extension("SPV_KHR_ray_tracing")
 		.build()?;
 	Ok(())
 }
